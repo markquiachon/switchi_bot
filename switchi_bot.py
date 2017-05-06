4615e460b8a1b87dae07c3fbbecff1a72c5c4822
Chive Quiachon <mark.quiachon@fullspeedtechnologies.com>
2017-05-06 19:08:07 +0800

    Added other route

--- switchi_bot.py
+++ switchi_bot.py
@@ -1,8 +1,14 @@
 from bottle import default_app, route, template
+import requests
 
 @route('/')
 def hello_world():
   return "Hello from Chive!"
+
+@route('/spread')
+def log_spreadsheet():
+  post_to = "https://script.google.com/macros/s/AKfycbz4AP5Xna4QStGbcX8eCC_9nryW03NJuA3qurLdo8J2uotvZ5c/exec"
+  r = requests.post(post_to, data={"test":"From Bottle"})
 
 @route('/hello/<name>')
 def index(name):
