f99acff2f91502cf4142adbf7029c84fe33839b6
chive <mark.quiachon@fullspeedtechnologies.com>
2017-05-08 17:21:39 +0900

    #

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bottle import request, default_app, route, template
 from slackclient import SlackClient
 from json import dumps, loads
 
+import wolframalpha
 import slack_bot
 import requests
 import os
@@ -10,6 +11,8 @@ import sys
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 VERIFICATION_TOKEN = os.environ.get("VERIFICATION_TOKEN") 
+SPREADSHEET_URL = os.environ.get("SPREADSHEET_URL")
+WOLFRAM_APP_ID = os.environ.get("WOLFRAM_APP_ID")
 BOT_NAME = "Switchi"
 
 switchi_bot = slack_bot.SlackBot(BOT_NAME,
@@ -34,28 +37,38 @@ def event_handler(event_type, event_json):
     user_id = event_json["event"].get("user")
     channel_id = event_json["event"].get("channel")
     command = event_json["event"].get("text").split(':')[0]
+    user_name = switchi_bot.get_user_name(user_id)
 
-    spreadsheet_url = os.environ.get("SPREADSHEET_URL")
     if command == "help":
-      get_url = spreadsheet_url + "?cmd=%s&state=%s" % (command, "help")
+      get_url = SPREADSHEET_URL + "?cmd=%s&state=%s" % (command, "help")
       response = requests.get(get_url)
       response = response.json() 
 
-      bot_response = "```"
+      bot_response = "@%s\n```" % user_name
       for cmd in response:
         bot_response = bot_response + "%s -> %s\n" % (cmd, response[cmd])
       bot_response = bot_response + "```"
 
       switchi_bot.post_channel_message(bot_response, channel_id, user_id)
     elif command == "log":
-      user_name = switchi_bot.get_user_name(user_id)
       bot_response = "@" + user_name + "\nMessage logged. Thanks! :smile:"
       message = event_json["event"].get("text").split(":")[1]
+      message = message.strip()
       status_code = log_spreadsheet(spreadsheet_url, message)
       
       if status_code == requests.codes.ok:
         switchi_bot.post_channel_message(bot_response, channel_id, user_id)
-
+    elif command == "ask":
+      input = event_json["event"].get("text").split(":")[1]
+      client = wolframalpha.Client(WOLFRAM_APP_ID) 
+      response = client.query(input)
+      bot_response = "@%s\n```" % user_name
+      for pod in response.pods:
+        for sub in pod.subpods:
+          bot_response = bot_response + sub.text + "\n"
+      bot_response = bot_response + "```"
+      
+      switchi_bot.post_channel_message(bot_response, channel_id, user_id) 
 
 
 @route('/auth_app')
