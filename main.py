197d60b14310aceb2e170328ab55c8dff2dbe9fe
chive <mark.quiachon@fullspeedtechnologies.com>
2017-05-08 12:41:46 +0900

    #

--- main.py
+++ main.py
@@ -35,8 +35,9 @@ def event_handler(event_type, event_json):
   if event_type == "message": 
     user_id = event_json["event"].get("user")
     channel_id = event_json["event"].get("channel")
-    command = event_json["event"].get("text").split(':')[0]
-    message = event_json["event"].get("text").split(':')[1]
+    #command = event_json["event"].get("text").split(':')[0]
+    #message = event_json["event"].get("text").split(':')[1]
+    command = event_json["event"].get("text")
 
     spreadsheet_url = os.environ.get("SPREADSHEET_URL")
     if command == "help":
